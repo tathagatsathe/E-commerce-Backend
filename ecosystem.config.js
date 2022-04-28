96d936ac79f447d1c7444bf3be637a44bbeafe81
Ubuntu <ubuntu@ip-172-31-14-87.ec2.internal>
2022-04-28 16:25:44 +0000

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name   : "server",
+    script : "./app.js"
+  }],
+  deploy: {
+    production: {
+	user: 'ubuntu',
+	host: 'ec2-3-234-225-87.compute-1.amazonaws.com',
+	key:  '~/.ssh/ecom.pem',
+	ref:  'origin/master',
+	repo: 'git@github.com:tathagatsathe/server.git',
+	path: '/home/ubuntu/server',
+	'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
